1633c807971fecc2ab1f8874de44d940f81fa2c0
jeandmalves <jeandmalves@gmail.com>
2023-11-09 11:55:13 -0300

    ta funcionando, mas a que custo?

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import register, index, cursed_word, show_cursed_words, proxy
+from .views import register, index, cursed_word, show_cursed_words, proxy, redirect_proxy
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', register, name='register'),
     path('words/', cursed_word, name='cursed_word'),
     path('list-words/', show_cursed_words, name='list_words'),
-    path('proxy/', proxy, name='proxy')
+    path('proxy/', proxy, name='proxy'),
+    path('redirect_proxy/', redirect_proxy, name="redirect_proxy")
 ]
--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from .forms import RegisterForm, CursedWordsForm, ProxyURLForm
 from .models import CursedWordsModel
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urlparse, urljoin, urlunparse
 
 def index(request):
     return render(request, 'index.html')
@@ -56,21 +57,97 @@ def show_cursed_words(request):
     return render(request, 'list_of_words.html', context=context)
 
 @login_required
-def proxy(request):
+def redirect_proxy(request):
     form = ProxyURLForm(request.POST or None)
     context = {
         'form':form
     }
     if str(request.method) == 'POST':
         if form.is_valid():
-            cursed_words = {
-                word.word:word.substitute_word for word in CursedWordsModel.objects.filter(id_user=request.user.id)
-            }
             url = form.cleaned_data["url"]
-            html = BeautifulSoup(requests.get(url).text, features="html.parser")
-            for word in cursed_words.keys():
-                palavra = html.find(text=word)
-                if palavra != None: 
-                    html.find(text=word).replaceWith(cursed_words[word])
-            return HttpResponse(html)
-    return render(request, 'proxy.html', context)
+            return redirect(f'http://127.0.0.1:8000/proxy?url={url}')
+    else:
+        return render(request, 'proxy.html', context)
+    
+
+@login_required
+def proxy(request):
+    if str(request.method) == 'GET':
+        url = request.GET.get('url', '')
+        request_html = requests.get(url)
+        soup = BeautifulSoup(request_html.text, features="html.parser")
+        html = apply_proxy_word_filter(soup=soup, id_user=request.user.id, url=url)
+        html = change_anchors_to_proxy(soup=soup, url=url)
+        return HttpResponse(str(html))
+    else:
+        return redirect('redirect_proxy')
+
+def change_anchors_to_proxy(soup, url):
+    parsed_url = urlparse(url)
+    base_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
+    for anchor in soup.find_all('a', href=True):
+        anchor_url = anchor['href']
+        absolute_url = urljoin(base_url, anchor_url)
+        anchor['href'] = f'http://127.0.0.1:8000/proxy?url={absolute_url}'
+    return soup
+
+def apply_proxy_word_filter(soup: str, id_user: int, url: str) -> BeautifulSoup:
+    # https://www.digitalocean.com/community/tutorials/getting-started-with-python-requests-get-requests
+
+    cursed_words = {
+        word.word:word.substitute_word for word in CursedWordsModel.objects.filter(id_user=id_user)
+    }
+    
+    for word in cursed_words.keys():
+        palavra = soup.find(text=word)
+        if palavra != None:
+            html.find(text=word).replaceWith(cursed_words[word])
+    
+    parsed_url = urlparse(url)
+    base_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
+
+    for link in soup.find_all('img', src=True):
+        asset_url = link['src']
+        absolute_url = urljoin(base_url, asset_url)
+        link['src'] = absolute_url
+        
+    for link in soup.find_all('link', href=True): # css
+        asset_url = link['href']
+        absolute_url = urljoin(base_url, asset_url)
+        asset_response = requests.get(absolute_url)
+        content_type = asset_response.headers.get('content-type', '').split(';')[0].strip()
+        if content_type == 'text/css':
+            new_tag = soup.new_tag('style', type='text/css')
+            new_tag.string = asset_response.text
+            link.replace_with(new_tag)
+        if content_type == 'text/javascript':
+            new_tag = soup.new_tag('script', type='text/javascript')
+            new_tag.string = asset_response.text
+            link.i.replace_with(new_tag)    
+    
+    for link in soup.find_all('script', src=True): # js    
+        # Muitos bloqueados pelo CORS MISSING ALLOW ORIGIN
+        asset_url = link['src']
+        absolute_url = urljoin(base_url, asset_url)
+        link['src'] = absolute_url
+        link['crossorigin'] = True
+    
+    # for link in soup.find_all('script', src=True): # js    
+    #     asset_url = link['src']
+    #     absolute_url = urljoin(base_url, asset_url)
+    #     asset_response = requests.get(absolute_url)
+    #     # content_type = asset_response.headers.get('content-type', '').split(';')[0].strip()
+    #     # if content_type == 'text/javascript':
+    #     try:
+    #         # new_tag = soup.new_tag('script', type='text/javascript')
+    #         # new_tag.string = asset_response.text
+    #         # new_tag['src'] = ''
+    #         # new_tag['href'] = ''
+            
+    #         link.string = asset_response.text
+    #         # link.i.replace_with(new_tag)    
+    #     except:
+    #         link['src'] = ''
+    #         link['href'] = ''
+    
+    return soup
